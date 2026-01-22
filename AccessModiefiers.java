2b8fc97ef7ee31a2e8eab04c6b3d8ccac2f36877
Arisetty Srinivas Jaswanth <jaswantharisetty@gmail.com>
2026-01-22 15:04:36 +0530

    Add files via upload

---
+++ AccessModiefiers.java
@@ -0,0 +1,15 @@
+class AccessModiefiers{
+
+private int id=255062;
+protected String name="jaswanth";
+int roll_no=62;
+public String section="CSE A";
+
+public static void main(String[] args){
+AccessModiefiers obj=new AccessModiefiers();
+System.out.println(obj.name);
+System.out.println(obj.roll_no);
+System.out.println(obj.section);
+}
+
+}
